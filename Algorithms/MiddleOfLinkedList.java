9a1f9590164abed14dc731a518a5aca4bd2d3f4f
SahilOlivet <btech10459.20@bitmesra.ac.in>
2021-10-02 00:16:08 +0530

    Middle of the Linked List

---
+++ Algorithms/MiddleOfLinkedList.java
@@ -0,0 +1,11 @@
+class MiddleOfLinkedList {
+    public ListNode middleNode(ListNode head) {
+        ListNode p1 = head;
+        ListNode p2 = head;
+        while (p2 != null && p2.next != null) {
+            p1 = p1.next;
+            p2 = p2.next.next;
+        }
+        return p1;
+    }
+}

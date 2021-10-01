5464f5c01e5ee4ead9ba1022751694802b673e26
yatender1199 <70021987+yatender1199@users.noreply.github.com>
2021-10-01 20:22:45 +0530

    Add files via upload

---
+++ PYTHON/xor.py
@@ -0,0 +1,15 @@
+t=int(input())
+def power(2,n-1,modulo):
+    
+    
+for i in range(0,t):
+    n=int(input())
+    
+    
+    c=b-1
+    d=c//2
+    if c%2==0:
+        print(d)
+        
+    else:
+        print(d+1)

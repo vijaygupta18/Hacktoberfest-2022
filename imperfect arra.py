6c9e6ed97330937095f4340ae87ac03e4769e257
ronniepratap <72915027+ronniepratap@users.noreply.github.com>
2021-10-01 12:38:01 +0530

    Create imperfect arra.py

---
+++ imperfect arra.py
@@ -0,0 +1,12 @@
+a,b,c=map(int,input().split())
+if a>b:
+    print('Takahashi')
+elif b>a:
+    print('Aoki')
+else:
+    if c==1:
+        print('Takahashi')
+    else:
+        print('Aoki')
+
+                

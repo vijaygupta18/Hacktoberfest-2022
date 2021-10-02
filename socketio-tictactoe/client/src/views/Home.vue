60955cf7a5bccfe9f02eedeff76e87900483dc42
anggidast <adastariana@gmail.com>
2021-10-02 18:49:33 +0700

    socketio-tictactoe

---
+++ socketio-tictactoe/client/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+
+    <EnterName></EnterName>
+
+  </div>
+</template>
+
+<script>
+import EnterName from '../components/EnterName.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    EnterName
+  },
+}
+</script>

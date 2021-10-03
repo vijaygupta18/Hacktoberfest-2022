172e22b94309cd6e955702fa5632acda778d7e76
kanishkagarwal6101 <kanishk6101@gmail.com>
2021-10-04 03:19:04 +0530

    full web app added

---
+++ ReactMusicSpotifyApi/src/Components/App/App.js
@@ -0,0 +1,87 @@
+import React from "react";
+import { SearchBar } from "../searchBar/SearchBar";
+import { SearchResults } from "../SearchResults/SearchResults";
+import { Playlist } from "../Playlist/playlist";
+import "./App.css";
+import { ReactDOM } from "react";
+
+import Spotify from "../../util/Spotify";
+
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [],
+      playlistName: "My playlist",
+      playlistTracks: [],
+    };
+
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
+  }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    if (tracks.find((savedTrack) => savedTrack.id === track.id)) {
+      return;
+    }
+    tracks.push(track);
+    this.setState({ playlistTracks: tracks });
+  }
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter((currentTrack) => currentTrack.id !== track.id);
+
+    this.setState({ playlistTracks: tracks });
+  }
+
+  updatePlaylistName(name) {
+    this.setState({ playlistName: name });
+  }
+
+  savePlaylist() {
+    const trackUris = this.state.playlistTracks.map((track) => track.uri);
+    Spotify.savePlaylist(this.state.playlistName, trackUris).then(() => {
+      this.setState({
+        playlistName: "New Playlist",
+        playlistTracks: [],
+      });
+    });
+  }
+
+  search(term) {
+    Spotify.search(term).then((searchResults) => {
+      this.setState({ searchResults: searchResults });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>
+          Ja<span className="highlight">mmm</span>ing
+        </h1>
+        <div className="App">
+          <SearchBar onSearch={this.search} />
+          <div className="App-playlist">
+            <SearchResults
+              searchResults={this.state.searchResults}
+              onAdd={this.addTrack}
+            />
+            <Playlist
+              playlistName={this.state.playlistName}
+              playlistTracks={this.state.playlistTracks}
+              onRemove={this.removeTrack}
+              onNameChange={this.updatePlaylistName}
+              onSave={this.savePlaylist}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
---
+++ ReactMusicSpotifyApi/src/Components/SearchResults/SearchResults.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./SearchResults.css";
+import { TrackList } from "../TrackList/TrackList";
+
+export class SearchResults extends React.Component {
+  render() {
+    return (
+      <div className="SearchResults">
+        <h2>Results</h2>
+        <TrackList
+          tracks={this.props.searchResults}
+          onAdd={this.props.onAdd}
+          isRemoval={false}
+        />
+      </div>
+    );
+  }
+}

418f972d62bf94ac1310d99beeb51cc5cc0913fe
Abhinav Kumar Gupta <84512702+alhad-balak@users.noreply.github.com>
2021-10-10 21:33:06 +0530

    Added Longest common subsequnce

---
+++ LongestCommonSubsequence.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+//Longest Common SubSequence
+#define f(i, a, b) for (int i = a; i < b; i++)
+#define vi vector<int>
+
+int solve(string s1, string s2, vector<vector<int>> &dp)
+{
+    int n = s1.size() + 1, m = s2.size() + 1;
+    f(i, 1, n)
+    {
+        f(j, 1, m)
+        {
+            if (s1[i - 1] == s2[j - 1])
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            else
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+
+    return dp[n - 1][m - 1];
+}
+int main()
+{
+
+    string s1, s2;
+    cin >> s1 >> s2;
+    int n = s1.size() + 1, m = s2.size() + 1;
+    vector<vector<int>> dp(n, vector<int>(m, 0));
+    cout <<"Longest Common SubSequence: "<< solve(s1, s2, dp) << endl;
+    return 0;
+}

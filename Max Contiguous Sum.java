e618f26be6cd3e2d2f2779a7cc9fbbe87469cb71
Francesco <88930554+FraM1000@users.noreply.github.com>
2021-10-16 16:27:19 +0200

    Update Max Contiguous Sum.java
    removed the int size variable because it was used just one time in the for , so i added the a.length in the for and in the System.out.println() at line 10 i added a space so it is better to read.

--- Max Contiguous Sum.java
+++ Max Contiguous Sum.java
@@ -7,16 +7,14 @@ class Kadane
 	public static void main (String[] args)
 	{
 		int [] a = {-2, -3, 4, -1, -2, 1, 5, -3};
-		System.out.println("Maximum contiguous sum is " +
-									maxSubArraySum(a));
+		System.out.println("Maximum contiguous sum is " + " " + maxSubArraySum(a));
 	}
 
 	static int maxSubArraySum(int a[])
 	{
-		int size = a.length;
 		int max_so_far = Integer.MIN_VALUE, max_ending_here = 0;
 
-		for (int i = 0; i < size; i++)
+		for (int i = 0; i < a.length; i++)
 		{
 			max_ending_here = max_ending_here + a[i];
 			if (max_so_far < max_ending_here)

aa001868c5019cf85bd520e8bab586f76f9cd0c0
Sachin Soni <64659289+striver-21@users.noreply.github.com>
2022-10-19 19:50:41 +0530

    Added Files

---
+++ bmi app/lib/constants/app_constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+final Color mainHexColor = HexColor("#1C1C1C");
+final Color accentHexColor = HexColor("#FCC91C");
